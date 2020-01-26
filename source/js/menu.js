fb16ac4e3d1a110f352b4cc46cbf204d68cb4cc0
Manul228 <qawaii@yandex.ru>
2020-01-26 06:54:24 +0300

    full

--- source/js/menu.js
+++ source/js/menu.js
@@ -11,6 +11,7 @@ navOpen.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    navOpen.classList.add('page-header__menu-open--disable');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
@@ -21,5 +22,6 @@ navClose.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--opened')) {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navOpen.classList.remove('page-header__menu-open--disable');
   }
 });
